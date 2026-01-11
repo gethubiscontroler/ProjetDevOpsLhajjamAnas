bea737a2ad55ba3864cb08b9ac5430d81b4ce918
gethubiscontroler <hattrius@uae.ac.ma>
2026-01-11 19:28:00 +0100

    app correction

--- myapp/src/main/java/com/example/myapp/controller/appController.java
+++ myapp/src/main/java/com/example/myapp/controller/appController.java
@@ -1,4 +1,4 @@
-package com.example.myapp.controller;
+package com.example.Myapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
